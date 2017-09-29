0067e98f3be6d569d5298bda3865b0573fe1f330
Alexander Cannon <a.m.t.cannon@gmail.com>
2017-09-29 17:28:22 +0100

    changed server port thingamy

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const axios = require('axios');
 const app = express();
 
-const port = 3608;
+const port = process.env.PORT ||  3608;
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
